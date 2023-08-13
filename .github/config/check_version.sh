d566f977838fc39821245fcc4a57a98219fc0f10
999 <999@hyeon.pro>
2023-08-13 19:26:22 +0900

    Create check_version.sh

---
+++ .github/config/check_version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# npm 버전과 package.json 버전이 같은지 아닌지 확인한다
+version=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+npm_version=$(npm view @Hansanghyeon/node-package-deploy version)
+
+# version이 npm_version보다 높아야한다
+if yarn semver "$version" -r ">$npm_version"; then
+    echo "true"
+else
+    echo "false"
+fi
